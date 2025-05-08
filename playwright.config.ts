633df4ea6ebb160475aba17c40859a98bd279da8
hernandosebastian <sebastianhernando.dev@gmail.com>
2025-05-08 17:11:04 -0300

    refactor: add tests locals

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,6 +22,14 @@ const config: PlaywrightTestConfig = {
         viewport: { width: 1280, height: 720 },
       },
     },
+    {
+      name: "local",
+      use: {
+        browserName: "chromium",
+        viewport: { width: 1280, height: 720 },
+        headless: true,
+      },
+    },
   ],
 };
 
