e81c89bb4eb8fbc1f5b24d34b489c17374d79f6d
hernandosebastian <sebastianhernando.dev@gmail.com>
2025-05-08 14:01:51 -0300

    tests: add certification tests

---
+++ tests/drag-and-drop-sliders.spec.ts
@@ -0,0 +1,56 @@
+import { test, expect, Page, Locator } from "@playwright/test";
+
+async function dragSliderToValue(
+  page: Page,
+  sliderLocator: Locator,
+  value: number
+) {
+  const sliderBox = await sliderLocator.boundingBox();
+
+  if (!sliderBox) {
+    return;
+  }
+
+  const sliderWidth = sliderBox.width;
+  const sliderX = sliderBox.x;
+  const sliderY = sliderBox.y;
+
+  const targetPositionX = sliderX + (sliderWidth * value) / 100;
+
+  await page.mouse.move(
+    sliderX + sliderWidth / 2,
+    sliderY + sliderBox.height / 2
+  );
+  await page.mouse.down();
+  await page.mouse.move(targetPositionX, sliderY + sliderBox.height / 2);
+  await page.mouse.up();
+}
+
+async function decreaseSliderValue(
+  page: Page,
+  sliderLocator: Locator,
+  targetValue: number
+) {
+  let value = await sliderLocator.inputValue();
+
+  while (parseInt(value) > targetValue) {
+    await page.mouse.move(5, 5);
+    await sliderLocator.press("ArrowLeft");
+    value = await sliderLocator.inputValue();
+  }
+}
+
+test("Drag & Drop Slider - moves slider to 95", async ({ page }) => {
+  await page.goto("https://www.lambdatest.com/selenium-playground");
+
+  await page.getByText("Drag & Drop Sliders").click();
+
+  const slider = page.locator('input[type="range"][value="15"]');
+
+  await dragSliderToValue(page, slider, 100);
+
+  await decreaseSliderValue(page, slider, 95);
+
+  const value = await slider.inputValue();
+  expect(value).toBe("95");
+});
